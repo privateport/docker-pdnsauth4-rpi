89a83c279499f13d17e34c255fd39f7e220dfe6b
Sneaky Scampi <admin@privateport.io>
2016-02-19 07:39:50 +0000

    update

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DB=/persistant/powerdns/powerdns.sqlite3
 if [ ! -f "$DB" ]; then
-        echo hi
-        sqlite3 $DB < /usr/share/doc/pdns-backend-sqlite3/schema.sqlite3.sql
+	mkdir -p /persistant/powerdns
+        sqlite3 $DB < /usr/local/share/doc/pdns/schema.sqlite3.sql 
 fi
 pdns_server
 
